27ed538744ddcced423f5cf799e3b9190cd6f813
Ilya Veselov <seaatsky.g@gmail.com>
2024-09-23 09:20:01 +0400

    WIP

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,17 @@ module ArmstrongWebServer
     config.i18n.available_locales = %i[en ru]
     config.i18n.default_locale = :ru
 
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework (
+      :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+      )
+    end
+
     config.after_initialize do
       next unless Server.table_exists?
 
